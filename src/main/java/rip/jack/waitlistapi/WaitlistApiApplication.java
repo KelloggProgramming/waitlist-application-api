9448d6725a91f0daee95ee07cd2ef8573c18f27a
Jack Kellogg <me@jack.rip>
2023-11-16 16:24:26 -0600

    Spring initializer base

---
+++ src/main/java/rip/jack/waitlistapi/WaitlistApiApplication.java
@@ -0,0 +1,13 @@
+package rip.jack.waitlistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitlistApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaitlistApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rip/jack/waitlistapi/WaitlistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rip.jack.waitlistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitlistApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
